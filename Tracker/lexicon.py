d21447a91909cf8f6acbddd39b11450cd20b3cc7
nightmare <arturkag2803@gmail.com>
2025-07-11 16:24:17 +0300

    Lexicon

---
+++ Tracker/lexicon.py
@@ -0,0 +1,30 @@
+MENU = {
+    "all": "All tasks",
+    "add task_name": "Add task",
+    "update task_id task_new_name": "Update task",
+    "delete task_id": "Delete task",
+    "mark-status task_id": "Change task's status"
+}
+
+LIST_MENU = {
+    "list": "list",
+    "list todo": "list todo",
+    "list in-progress": "list in-progress",
+    "list done": "list done"
+}
+
+FUNCS = {
+    "list": "list",
+    "list-all": "all",
+    "add": "add",
+    "update": "update",
+    "delete": "delete",
+    "in-progress": "mark-in-progress",
+    "done": "mark-done",
+    "list done": "list done",
+    "list in-progress": "list in-progress",
+    "list todo": "list todo",
+    "mark-done": "mark-done",
+    "mark-in-progress": "mark-in-progress",
+    "exit": "exit"
+}
