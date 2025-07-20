421f6eefe07741e5760c253f040d7e13731dce3f
nightmare <arturkag2803@gmail.com>
2025-07-20 10:39:45 +0300

    Add files via upload

---
+++ Finance tracker/lexicon.py
@@ -0,0 +1,13 @@
+FUNCS = {
+    "description": "--description",
+    "amount": "--amount",
+    "add": "add",
+    "exit": "exit",
+    "list": "list",
+    "summary": "summary",
+    "delete": "delete",
+    "month": "--month",
+    "id": "--id",
+    "list": "list",
+    "summary": "summary"
+}
